c9d132df2605a55c5cf3806320e90aa476ebd39e
pranyagitech <pranya.ag@gmail.com>
2025-11-14 20:18:33 +0530

    Adding leap year checker program

---
+++ Conditional Statements/To check whether a year is leap or not.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    //To Find if the entered year is leap or not
+    int year;
+    printf("Enter the year: ");
+    scanf("%d", &year);
+    
+    if (year % 4 == 0) {
+        printf("It is a leap year");
+    } else {
+        printf("It is not a leap year");
+    }
+    
+    return 0;
+}
+
