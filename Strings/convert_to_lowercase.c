51033fcd7477e26ffc9b523b12a9df3f1397dae9
pranyagitech <pranya.ag@gmail.com>
2026-02-13 23:22:54 +0530

    Add function to manually convert string to lowercase

---
+++ Strings/convert_to_lowercase.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+char str[100];
+
+fgets (str, 100, stdin);
+
+int i = 0;
+while(str[i] != '\0')
+{
+if (str[i] >= 'A' && str[i] <= 'Z')
+{
+str[i] = str[i] +32;
+}
+i++;
+}
+
+printf("The lowercased string is &s", str);
+
+return 0;
+}
